caddda317fd13c58e749782f601e90b8fef9fdd9
RustamKZZ <kzrustam@gmail.com>
2022-06-02 19:30:14 +0300

    #30 AlertDialog//

--- app/src/main/java/com/hrv/fintessapp/utils/FragmentManager.kt
+++ app/src/main/java/com/hrv/fintessapp/utils/FragmentManager.kt
@@ -9,6 +9,7 @@ object FragmentManager {
 
     fun setFragment(newFragment: Fragment, act: AppCompatActivity){
         val transaction = act.supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
         transaction.replace(R.id.placeHolder, newFragment)
         transaction.commit()
         currentFragment = newFragment
